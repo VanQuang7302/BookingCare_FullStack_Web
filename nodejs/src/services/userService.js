ae724505ee38b3f735dce2829dd4bbb1b3942913
quang.pv <quang.pv@tinasoft.com.vn>
2024-02-26 12:22:44 +0700

    done62

--- nodejs/src/services/userService.js
+++ nodejs/src/services/userService.js
@@ -132,8 +132,10 @@ let CreateNewUser =(data) =>{
                 lastName: data.lastName,
                 address: data.address,
                 phonenumber: data.phonenumber,
-                gender: data.gender === '1' ? true : false,
+                gender: data.gender,
                 roleId: data.roleId,
+                positionId:data.positionId,
+                image:data.avatar
             })
             resolve({
                 errCode: 0,
@@ -175,7 +177,7 @@ let deleteUser = (userId) =>{
 let updateUserData = (data) =>{
     return new Promise(async(resolve,reject)=>{
         try{
-            if(!data.id){
+            if(!data.id ||!data.roleId ||!data.gender ||!data.positionId   ){
                 resolve({
                     errCode:2,
                     errMessage:'Missing required parameters'
@@ -190,7 +192,13 @@ let updateUserData = (data) =>{
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
-
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phonenumber = data.phonenumber;
+                if(data.avatar){
+                    user.image =data.avatar;
+                }
                 await user.save();
                 
                 resolve({
